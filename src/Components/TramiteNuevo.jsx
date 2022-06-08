bf93e03930c509b08930aed4c8878876e403a3d6
kendallCloud <kendall24gb@gmail.com>
2022-06-07 20:38:46 -0600

    JsonTramite

--- src/Components/TramiteNuevo.jsx
+++ src/Components/TramiteNuevo.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function TramiteNuevo({departamentos}){
+function TramiteNuevo({departamentos,setData}){
 
 const [archivos,setarchivos] = useState([]);
 const [extension,setExtension] = useState([]);
@@ -9,15 +9,11 @@ const [realiza,setRealiza] = useState("");
 const [aux,setaux] = useState("");
 const [tipo,setipo] = useState("");
 
-let json = {"archivo":archivos};
-const[datos,setDatos] = useState(json);
-console.log(json);
-
   return(
     <form className="modal-card-body">
-    <input type="text" className="input" placeholder="Nombre del tramite" /><br/>
+    <input type="text" className="input" name="name" onChange={(event) =>{setData(event.target.name,event.target.value)}} placeholder="Nombre del tramite" /><br/>
      <div className="select">
-       <select   onChange={(event) =>{setRealiza(event.target.value); console.log(realiza);}}>
+       <select  name="depart" onChange={(event) =>{setData(event.target.name,event.target.value)}}>
        <option value="default">
           Departamento que lo realiza.
         </option>
@@ -74,8 +70,6 @@ console.log(json);
              })
         }
          </p>
-
-         <button className="button is-success">Guardar cambios</button>
 </form>);
 
 }
@@ -86,15 +80,30 @@ class Tramite extends React.Component {
         this.state = {
           list:["gerencia","secretaria"],
           aux:"",
-          ver: this.props.active?"is-active":""
+          ver: this.props.active?"is-active":"",
+          form:{
+            aprobar:[],
+            name:"X",
+            documentos:[],
+            TipoArchivos:[],
+            depart:"z"
+         }
         };
         this.metodo=this.metodo.bind(this);
-    }
+        this.PuenteDatos=this.PuenteDatos.bind(this)}
 
     metodo(){ 
       this.state.ver===""?this.setState({ver:"is-active"}):this.setState({ver:""})
       console.log(this.state.ver);
     }
+
+    PuenteDatos(key,value){
+      console.log(key+" value "+value);
+     var objeto = this.state.form;
+     objeto[key] = value;
+     this.setState({form:objeto});
+     console.log(this.state.form);
+   }
     
     render(){
         return(
@@ -105,8 +114,9 @@ class Tramite extends React.Component {
             <p className="modal-card-title">Modificar tramite</p>
             <button className="delete" aria-label="close" />
             </header>
-            <TramiteNuevo departamentos={this.props.departamentos}  />
+            <TramiteNuevo departamentos={this.props.departamentos} setData={this.PuenteDatos} />
             <footer className="modal-card-foot">
+            <button className="button is-success">Guardar cambios</button>
             <button className="button"  onClick={this.metodo}>Cancelar</button>
             </footer>
         </div>
